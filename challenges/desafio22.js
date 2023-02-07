6215dfb903d3fdf88b3f491309d7f19ce96f9a70
Windson Macedo <windsondev@hotmail.com>
2023-02-07 18:18:01 -0300

    feat: requisito 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,7 @@
+db.produtos.updateMany({}, { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } });
+
+db.produtos.updateMany({ tags: { $all: ["bovino"] } }, { $inc: { "vendasPorDia.6": 120 } });
+
+db.produtos.updateMany({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
+
+db.produtos.find({}, { nome: 1, _id: 0, vendasPorDia: 1 });
